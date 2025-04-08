b537127608bc841a0c20e9a9ae669e5c2eb1bc67
Michel LAO <SansNom01@proton.me>
2025-04-08 20:17:13 +0200

    Create a custom S3Client for Controller

---
+++ src/main/kotlin/com/example/shorturl/datasource/S3ClientData.kt
@@ -0,0 +1,35 @@
+package com.example.shorturl.datasource
+
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Component
+import software.amazon.awssdk.core.sync.RequestBody
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.PutObjectRequest
+import software.amazon.awssdk.services.s3.model.PutObjectResponse
+import java.io.InputStream
+
+@Component
+class S3ClientData(private val s3Client: S3Client) {
+    val bucketName = "shorturl"
+
+    fun readData(k: String): ResponseEntity<ByteArray> {
+        val response = s3Client.getObject { request ->
+            request.bucket(this.bucketName).key(k)
+        }
+        val objectMetadata = response.response()
+        val contentType = MediaType.valueOf(objectMetadata.contentType() ?: MediaType.APPLICATION_OCTET_STREAM_VALUE)
+        return ResponseEntity.ok()
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=$k")
+            .contentType(contentType)
+            .body(response.readAllBytes())
+    }
+
+    fun writeData(key: String, inputStream: InputStream, contentType: String): PutObjectResponse? {
+        val putObjectRequest = PutObjectRequest.builder().bucket(this.bucketName).key(key).contentType(contentType).build()
+        val requestBody = RequestBody.fromBytes(inputStream.readBytes())
+        val response = s3Client.putObject(putObjectRequest, requestBody)
+        return response
+    }
+}
