b136176e933b59f2e8d79cd8c4ea8c81a54b37c9
Michel LAO <SansNom01@proton.me>
2025-04-15 20:46:21 +0200

    Add class to generate a random path url

---
+++ src/main/kotlin/com/example/shorturl/UrlHandler.kt
@@ -0,0 +1,17 @@
+package com.example.shorturl
+
+class UrlHandler(private val url: String) {
+    companion object {
+        private const val characters = "abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ-_!*'"
+
+        fun generatorURL(nbChar: Int): String {
+            var strings = ""
+            for (i in 0..nbChar) {
+                strings += characters.random()
+            }
+            return strings
+        }
+    }
+
+    fun getDotExtensionFile() = "." + url.substringAfterLast('.')
+}
