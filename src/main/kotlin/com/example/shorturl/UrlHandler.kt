0472bc97307a7bc05a18c27839803686e27dbedf
Michel LAO <SansNom01@proton.me>
2025-04-17 15:48:56 +0200

    set const upper case name

--- src/main/kotlin/com/example/shorturl/UrlHandler.kt
+++ src/main/kotlin/com/example/shorturl/UrlHandler.kt
@@ -2,12 +2,12 @@ package com.example.shorturl
 
 class UrlHandler(private val url: String) {
     companion object {
-        private const val characters = "abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ-_!*0123456789"
+        private const val CHARACTERS = "abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ-_!*0123456789"
 
         fun generatorURL(nbChar: Int): String {
             var strings = ""
             for (i in 0..nbChar) {
-                strings += characters.random()
+                strings += CHARACTERS.random()
             }
             return strings
         }
