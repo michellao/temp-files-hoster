8076ee89a4d8e73d19f4bd83dbfd6efc48d78400
Michel LAO <SansNom01@proton.me>
2025-04-17 15:45:56 +0200

    Add digit number

--- src/main/kotlin/com/example/shorturl/UrlHandler.kt
+++ src/main/kotlin/com/example/shorturl/UrlHandler.kt
@@ -2,7 +2,7 @@ package com.example.shorturl
 
 class UrlHandler(private val url: String) {
     companion object {
-        private const val characters = "abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ-_!*'"
+        private const val characters = "abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ-_!*0123456789"
 
         fun generatorURL(nbChar: Int): String {
             var strings = ""
