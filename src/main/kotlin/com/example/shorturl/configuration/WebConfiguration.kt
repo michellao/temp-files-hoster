e3dcf4568b7a5f719fb758b970a2d1071c57850b
Michel LAO <SansNom01@proton.me>
2025-04-26 21:33:20 +0200

    Create the index.html and the route to access it

---
+++ src/main/kotlin/com/example/shorturl/configuration/WebConfiguration.kt
@@ -0,0 +1,17 @@
+package com.example.shorturl.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.CacheControl
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.util.concurrent.TimeUnit
+
+@Configuration
+class WebConfiguration : WebMvcConfigurer {
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/resources/**")
+            .addResourceLocations("classpath:/public/")
+            .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS))
+    }
+}
+
--- src/main/kotlin/com/example/shorturl/controller/UrlController.kt
+++ src/main/kotlin/com/example/shorturl/controller/UrlController.kt
@@ -10,8 +10,12 @@ import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
 class UrlController(private val service: UrlService, private val s3: S3ClientData) {
+    @GetMapping("/")
+    fun index() = "forward:/resources/index.html"
+
     @GetMapping("/{generatedUrl}", produces = [MediaType.TEXT_PLAIN_VALUE, MediaType.APPLICATION_OCTET_STREAM_VALUE])
     fun getFile(@PathVariable("generatedUrl") generatedUrl: String): ResponseEntity<ByteArray> {
+        println("Generated url: $generatedUrl")
         service.findByUrl("/$generatedUrl")?.let { url ->
             val rawData = s3.readData(url)
             return rawData
