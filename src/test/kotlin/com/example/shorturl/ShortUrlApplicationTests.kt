c75d7dd6704b73be6ae3ab5f7a749bdf4acffe34
Michel LAO <SansNom01@proton.me>
2025-05-09 21:23:56 +0200

    Set up test for assert the path /

--- src/test/kotlin/com/example/shorturl/ShortUrlApplicationTests.kt
+++ src/test/kotlin/com/example/shorturl/ShortUrlApplicationTests.kt
@@ -1,13 +1,21 @@
 package com.example.shorturl
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.assertj.MockMvcTester
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableAutoConfiguration
+@AutoConfigureTestDatabase
+@AutoConfigureMockMvc
 class ShortUrlApplicationTests {
-
 	@Test
-	fun contextLoads() {
+	fun testLoadingIndexPage(@Autowired mvc: MockMvcTester) {
+		assertThat(mvc.get().uri("/")).hasStatusOk()
 	}
-
 }
