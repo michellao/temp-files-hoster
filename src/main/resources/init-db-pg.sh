d918aef68839753a7b38993c270fd1640e12f440
Michel LAO <SansNom01@proton.me>
2025-05-21 19:53:24 +0200

    fix path schema.sql

--- src/main/resources/init-db-pg.sh
+++ src/main/resources/init-db-pg.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -e
 createdb --username $POSTGRES_USER $DATABASE_NAME
-cat ../schema.sql | psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER --dbname $DATABASE_NAME
+cat schema.sql | psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER --dbname $DATABASE_NAME
