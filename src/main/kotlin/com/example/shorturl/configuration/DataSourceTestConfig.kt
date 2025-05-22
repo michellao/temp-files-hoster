f6249d92defe30dd01ddffccb4bf95826d06c094
Michel LAO <SansNom01@proton.me>
2025-05-22 19:54:21 +0200

    Unit test with it own embed database

---
+++ src/main/kotlin/com/example/shorturl/configuration/DataSourceTestConfig.kt
@@ -0,0 +1,22 @@
+package com.example.shorturl.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.context.annotation.Profile
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
+import javax.sql.DataSource
+
+@Configuration
+@Profile("test")
+class DataSourceTestConfig {
+    @Bean
+    @Primary
+    fun dataSource(): DataSource {
+        return EmbeddedDatabaseBuilder()
+            .setType(EmbeddedDatabaseType.H2)
+            .addScript("schema.sql")
+            .build()
+    }
+}
--- src/test/kotlin/com/example/shorturl/ShortUrlApplicationTests.kt
+++ src/test/kotlin/com/example/shorturl/ShortUrlApplicationTests.kt
@@ -3,16 +3,16 @@ package com.example.shorturl
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.assertj.MockMvcTester
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@EnableAutoConfiguration
-@AutoConfigureTestDatabase
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class ShortUrlApplicationTests {
 	@Test
 	fun testLoadingIndexPage(@Autowired mvc: MockMvcTester) {
