f9f47193a66176fab07d838a8bb9ccaf3cb15819
Michel LAO <SansNom01@proton.me>
2025-06-05 20:06:20 +0200

    test: on uploading

---
+++ src/test/kotlin/com/example/shorturl/FileUploadTests.kt
@@ -0,0 +1,43 @@
+package com.example.shorturl
+
+import com.example.shorturl.configuration.properties.MyAppProperties
+import jakarta.servlet.ServletContext
+import org.hamcrest.core.StringStartsWith
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.mock.web.MockMultipartFile
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class FileUploadTests(@Autowired private val myApp: MyAppProperties) {
+    @Autowired
+    private lateinit var servletContext: ServletContext
+    @Test
+    fun testUpload(@Autowired mvc: MockMvc) {
+        val file = MockMultipartFile(
+            "file",
+            "fileTest.txt",
+            "text/plain",
+            "Hello world".toByteArray()
+        )
+        mvc.perform {
+            multipart("/")
+                .file(file)
+                .buildRequest(servletContext)
+        }
+            .andExpect(status().isOk)
+            .andExpect {
+                content().string(StringStartsWith(myApp.bucketName))
+            }
+    }
+}
