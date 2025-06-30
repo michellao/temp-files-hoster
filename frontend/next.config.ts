6016d4d4731b75f68bb0d0b4bf88c8e8fed09927
Michel LAO <SansNom01@proton.me>
2025-06-30 20:08:47 +0200

    Add zod, set typescript in strict mode, required an env var

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,13 @@
 import type { NextConfig } from "next";
 
+const requiredEnvVars = ['NEXT_PUBLIC_BACKEND_URL'];
+
+requiredEnvVars.forEach((envVar) => {
+  if (!process.env[envVar]) {
+    throw new Error(`Missing required environment variable: ${envVar}`);
+  }
+});
+
 const nextConfig: NextConfig = {
   devIndicators: false,
   output: 'export'
