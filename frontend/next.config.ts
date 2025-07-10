cde6c8d0656fcc039e808764945ee88707d37db0
Michel LAO <SansNom01@proton.me>
2025-07-10 16:51:43 +0200

    frontend: add optional customisable setup Sentry for analytics

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import { withSentryConfig } from "@sentry/nextjs";
 
 const requiredEnvVars = ['NEXT_PUBLIC_BACKEND_URL'];
 
@@ -8,9 +9,20 @@ requiredEnvVars.forEach((envVar) => {
   }
 });
 
-const nextConfig: NextConfig = {
+let nextConfig: NextConfig = {
   devIndicators: false,
   output: 'export'
 };
+
+if (process.env.SENTRY_ORG && process.env.SENTRY_PROJECT && process.env.NEXT_PUBLIC_SENTRY_DSN) {
+  nextConfig = withSentryConfig(nextConfig, {
+    org: process.env.SENTRY_ORG,
+    project: process.env.SENTRY_PROJECT,
+
+    silent: !process.env.CI,
+
+    disableLogger: true,
+  });
+}
 
 export default nextConfig;
