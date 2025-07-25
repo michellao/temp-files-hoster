a0052ccc92730e4835d42a1fae193c60c3c68247
Michel LAO <SansNom01@proton.me>
2025-07-25 19:22:31 +0200

    app: Accepted archive

--- src/main/kotlin/com/example/shorturl/datasource/MimeType.kt
+++ src/main/kotlin/com/example/shorturl/datasource/MimeType.kt
@@ -4,6 +4,8 @@ enum class MimeType(val data: String) {
     APPLICATION_PDF("application/pdf"),
     APPLICATION_OCTET_STREAM("application/octet-stream"),
     TEXT_PLAIN("text/plain"),
+    APPLICATION_ZIP("application/zip"),
+    APPLICATION_X_7Z_COMPRESSED("application/x-7z-compressed"),
     APPLICATION_JSON("application/json");
 
     override fun toString(): String = data
