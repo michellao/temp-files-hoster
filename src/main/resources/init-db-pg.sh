cb7c94893e28d0832cd93440c08efff92d9219f6
Michel Lao <SansNom01@proton.me>
2025-07-28 14:59:34 +0200

    Delete type enum mime and using builtin create db

--- src/main/resources/init-db-pg.sh
+++ src/main/resources/init-db-pg.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env sh
 set -e
-createdb --username $POSTGRES_USER $DATABASE_NAME
 cat schema.sql | psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER --dbname $DATABASE_NAME
-psql --username $POSTGRES_USER --dbname $DATABASE_NAME << EOF
-CREATE CAST (character varying AS mime) with inout as assignment;
-EOF
