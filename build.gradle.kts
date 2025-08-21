e70d63f90d19afd0ef663233fd1c22e19c4d8491
Michel Lao <SansNom01@proton.me>
2025-08-21 17:16:28 +0200

    app: upgrade spring boot v3.5.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "2.2.10"
     kotlin("plugin.spring") version "2.2.10"
-    id("org.springframework.boot") version "3.5.4"
+    id("org.springframework.boot") version "3.5.5"
     id("io.spring.dependency-management") version "1.1.7"
     id("com.gorylenko.gradle-git-properties") version "2.4.1"
 }
